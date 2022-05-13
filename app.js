0ca27e758ccb6397b305abd381ba1f3ebc6af56f
nserbou <naim.serbouh@gmail.com>
2022-05-13 15:49:46 +0200

    UPLOAD FINAL VERSION

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+function displayWeather(meteos_semaine, nbjour)
+{
+
+    const formDisplayResult = document.getElementById('formResult')                
+    formDisplayResult.innerHTML = []
+   
+    var uviCheck =  getTheme(meteos_semaine[7]);
+
+    for (var i = 0; i < nbjour; i++)
+    {
+        const dayValue = getDayNumber().week[getDayNumber().currentDay + i]
+                   
+        const dayDiv = document.createElement('div')
+        dayDiv.innerHTML = dayValue
+        
+        const iconeWeather = document.createElement('img')
+
+    var iconeDisplayId = meteos_semaine[i];
+
+    if (iconeDisplayId == 803 || iconeDisplayId == 804) {
+        console.log(iconeDisplayId)
+        if (uviCheck)  {
+            iconeWeather.src = "./img/clouds-white.png"
+        } else iconeWeather.src = "./img/clouds.svg"
+
+    }
+    else if (iconeDisplayId == 801 || iconeDisplayId == 802) {
+        console.log(iconeDisplayId)
+        if (uviCheck) {
+            iconeWeather.src = "./img/cloudy_white.png"
+        } else iconeWeather.src = "./img/cloudy.svg"
+    }
+    else if (iconeDisplayId <= 622 && iconeDisplayId >= 600) {
+        console.log(iconeDisplayId)
+        if (uviCheck) {
+            iconeWeather.src = "./img/snow-white.png"
+        } else iconeWeather.src = "./img/snow.svg"
+    }
+    else if (iconeDisplayId == 800) {
+        console.log(iconeDisplayId)
+        if (uviCheck) {
+            iconeWeather.src = "./img/sun-white.png"
+        } else iconeWeather.src = "./img/sun.svg"
+    }   
+    else {
+        console.log(iconeDisplayId)
+        if (uviCheck) {
+            iconeWeather.src = "./img/rain-white.png"
+        } else iconeWeather.src = "./img/rain.svg"
+    }
+    formDisplayResult.appendChild(dayDiv)
+    dayDiv.appendChild(iconeWeather)
+    }
+}
+
 
 function getCoord(data) {
     const lat = data.results[0].geometry.lat
@@ -31,6 +86,8 @@ function getDayNumber(){
 
 document.addEventListener("DOMContentLoaded", function() {
     
+    monstockage = localStorage;
+
     const submitButton = document.getElementById("submit")
     
     const API_KEY_loc = "78f0265465d04dbeb14f38246509912d"
@@ -41,6 +98,16 @@ document.addEventListener("DOMContentLoaded", function() {
         
         const location = document.getElementById("userLocation")
         const userLocation = location.value
+
+        const lavilleexiste = monstockage.getItem(userLocation);
+
+        if (lavilleexiste)
+        {
+            displayWeather(JSON.parse(lavilleexiste), document.getElementById("day-select").value);
+            console.log("ca marche");
+        }
+        else
+        {
         let URL = `https://api.opencagedata.com/geocode/v1/json?q=${userLocation}&key=${API_KEY_loc}&language=fr&pretty=1`
         
         fetch(URL) 
@@ -70,7 +137,19 @@ document.addEventListener("DOMContentLoaded", function() {
                 formDisplayResult.innerHTML = []
                 const daysToDisplay = document.getElementById("day-select").value
 
-                for (let i= 0; i < daysToDisplay ; i++) {
+                var codes_meteo = [];
+
+                for (let i = 0; i < 7; i++)
+                {
+                    codes_meteo[i] = cityWeather.daily[i].weather[0].id;
+                }
+                
+                codes_meteo[7] = cityWeather.current.uvi;
+                monstockage.setItem(userLocation, JSON.stringify(codes_meteo));
+
+                displayWeather(codes_meteo, daysToDisplay);
+
+                /*for (let i= 0; i < daysToDisplay ; i++) {
                    const dayValue = getDayNumber().week[getDayNumber().currentDay + i]
                    
                    const dayDiv = document.createElement('div')
@@ -116,11 +195,11 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
                     formDisplayResult.appendChild(dayDiv)
                     dayDiv.appendChild(iconeWeather)
-                }
+                }*/
             })
             .catch(err=> console.log(err))
             })
             .catch(err=> console.log(err))
 
-    });
+    }});
 });
