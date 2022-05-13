f62bcb25d81d5ac64e76446dbcdb6eace0150799
nserbou <naim.serbouh@gmail.com>
2022-05-13 09:41:47 +0200

    Final Version

--- forcast.js
+++ forcast.js
@@ -1,77 +1,135 @@
-document.addEventListener("DOMContentLoaded", function() 
-{   
+// ########## FONCTIONS ##########
+
+function getCoord(data) {
+    const lat = data.results[0].geometry.lat
+    const lon = data.results[0].geometry.lng
+    return { lat, lon }
+}
+
+// THEME MODE FUNCTION
+function getTheme(uv) {
+    if (uv == 0) {
+        uviCheck = true
+        document.getElementById("body").style.transition = "all 1s";
+        document.querySelector("body").style.background = "#06145F";  
+        document.querySelector("body").style.color = "#FFFFFF";  
+    } else {
+        uviCheck = false
+        document.getElementById("body").style.transition = "all 1s";
+        document.querySelector("body").style.background = "#3399FF"
+        document.querySelector("body").style.color = "#000000";  
+    }
+    return uviCheck
+}
+
+// WEEK ARRAY AND CURRENT DAY FOR FUTUR CALCUL
+function getDayNumber(){
+    const currentDate = new Date();
+    const currentDay = currentDate.getDay()
+    const week = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi",
+                "Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
+    return { week, currentDay }
+    }   
+
+
+// ########## DEROULE LOGIQUE ###############
+
+document.addEventListener("DOMContentLoaded", function() {
+    //--------------------------------------------
     
-    let submit = document.getElementById("submitCity")    
-    const newDate = new Date();
-    const day = newDate.getDay()
+    const submitButton = document.getElementById("submit")
     
-   
-    submit.addEventListener("click",function(){
-        let today = document.getElementsByTagName("p")[0]
-        if (day=="1")
-        {
-            today.innerHTML="monday"
-        }
-        else if(day=="2")
-        {
-            today.innerHTML="tuesday"
-        }
-        else if(day=="3")
-        {
-            today.innerHTML="wednesday"
-        }
-        else if(day=="4")
-        {
-            today.innerHTML="thursday"
-        }
-        else if (day=="5")
-        {
-            today.innerHTML="friday"
-        }
-        else if(day=="6")
-        {
-            today.innerHTML="saturday"
-        }
-        else if (day=="7")
-        {
-            today.innerHTML="sunday"
-        }
-        let city = document.getElementById("addCity").value
-        console.log(city);
-        const API_KEY = "dd9a3565602c437d9dd68c5e4f065470"
-        let URL = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${API_KEY}&language=fr&pretty=1`
-            
-         fetch(URL) // on utilise la methode fetch, qui est asynchrone et qui existe par défaut dans le navigateur (on aurait aussi pu utiliser la librairie axios par exemple)
-    // on utilise la méthode then() (NB: on pourrait aussi utiliser la syntaxe async/await)
+    const API_KEY_loc = "9d4b2106584d4236b68d77703e0ec133"
+    const API_KEY_weather = "6c601f4c97c69803c3d0ea71c97c199e"
+    
+    submitButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        
+        const location = document.getElementById("userLocation")
+        const userLocation = location.value
+        let URL = `https://api.opencagedata.com/geocode/v1/json?q=${userLocation}&key=${API_KEY_loc}&language=fr&pretty=1`
+        
+        fetch(URL) 
         .then(response => { 
-        if (response.status == 200) { // on vérifier que l'appel à l'API a fonctionné
-            return response.json()  // ne pas oublier le return du callback
-        }
-        else console.log(`Erreur lorsqu'on a tenté de récupérer les data`);
+            if (response.status == 200) { 
+                return response.json()
+            }
+            else console.log(`Erreur lorsqu'on a tenté de récupérer les data`);
         })
         .then(data => {
-        const lattitude=data.results[0].geometry.lat
-        const longitude=data.results[0].geometry.lng;
-        const API_KEY2="104bf1cd8898dd5511abb9ccefbc7b63"
-        URL2=`https://api.openweathermap.org/data/2.5/onecall?lat=${lattitude}&lon=${longitude}&appid=${API_KEY2}`
-        return fetch(URL2)
-                   .then(response => response.json())
-                   .then(data=>{
-                    let currentDay = data.current.weather[0].id
-                    let bigIcon = getElementById("icon")
-                    if (currentDay = 800) {
-                        bigIcon.innerHTML = <Image src= "./images/clouds"></Image>
+           return getCoord(data)
+        }) 
+        .then(coords => {
+            let URL_weather = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude={part}&appid=${API_KEY_weather}`
+            console.log(URL_weather)
+            
+            return fetch(URL_weather)
+            .then(response => { 
+                if (response.status == 200) { 
+                    return response.json()
+                }
+                else console.log(`Erreur lorsqu'on a tenté de récupérer les data Weather`);
+            })
+            .then(cityWeather => { 
+                // reset le tableau au debut
+                // ask for user input number of days to display
+                const formDisplayResult = document.getElementById('formResult')                
+                formDisplayResult.innerHTML = []
+                const daysToDisplay = document.getElementById("day-select").value
+
+                for (let i= 0; i < daysToDisplay ; i++) {
+                   // Appel function getTheme et return uviCheck en True ou False
+                   const dayValue = getDayNumber().week[getDayNumber().currentDay + i]
+                   
+                   const dayDiv = document.createElement('div')
+                   dayDiv.innerHTML = dayValue
+                   
+                   const iconeWeather = document.createElement('img')
+
+                   let uviCheck = false
+                   uviCheck = getTheme(cityWeather.current.uvi)
+
+                    let iconeDisplayId = cityWeather.daily[i].weather[0].id
+
+                    if (iconeDisplayId == 803 || iconeDisplayId == 804) {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/clouds-white.png"
+                        } else iconeWeather.src = "./img/clouds.svg"
+
                     }
+                    else if (iconeDisplayId == 801 || iconeDisplayId == 802) {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/cloudy_white.png"
+                        } else iconeWeather.src = "./img/cloudy.svg"
+                    }
+                    else if (iconeDisplayId <= 622 && iconeDisplayId >= 600) {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/snow-white.png"
+                        } else iconeWeather.src = "./img/snow.svg"
+                    }
+                    else if (iconeDisplayId == 800) {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/sun-white.png"
+                        } else iconeWeather.src = "./img/sun.svg"
+                    }   
+                    else {
+                        console.log(iconeDisplayId)
+                        if (uviCheck) {
+                            iconeWeather.src = "./img/rain-white.png"
+                        } else iconeWeather.src = "./img/rain.svg"
+                    }
+                    formDisplayResult.appendChild(dayDiv)
+                    dayDiv.appendChild(iconeWeather)
+                }
+            })
+            .catch(err=> console.log(err))
+            })
+            .catch(err=> console.log(err))
 
-
-                    })
-                   .catch(err => {
-                   console.error('Request failed', err)
-        })
-        
-        })
-        
-       
-        
-    })   
-})
+    });
+    //--------------------------------------------
+});
